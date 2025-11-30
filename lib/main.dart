2feccb52e53187aa67d56172d8c696ae179a8a37
Rafael Leonan <faelleonan@gmail.com>
2025-11-30 11:43:21 -0300

    fix: Ajuste 24

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'routes/app_router.dart';
 import 'firebase_options.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  usePathUrlStrategy();
   runApp(const MyApp());
 }
 
